61f4753a1e97ca227177c857877c75921669ad06
0x6b6579 <49789253+th3k3y@users.noreply.github.com>
2021-04-03 18:04:34 +0200

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -1,35 +1,14 @@
 import socket
-from sys import exit
-import colorama
-from colorama import Back, Fore, Style, deinit, init
-init()
-
-print(Fore.MAGENTA)
-ip = input("\nTarget IP : ")
-print(Fore.WHITE + "\n-------")
-print(Fore.MAGENTA)
-ports = input("Enter ports to scan : ")
-print(Fore.WHITE + "\n-------")
-ports = ports.replace(' ', '')
-
-for port in ports.split(','):
-	try:
-		port = int(port)
-	except:
-		print(Fore.YELLOW)
-		print("[*]Incorrect.")
-		exit(1)
-
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	code = sock.connect_ex((ip, port))
-
-	if code==0 :
-		print(Fore.GREEN)
-		print("[*]Port {} is open !".format(str(port)))
-	else:
-		print(Fore.RED)
-		print("[*]Port {} is closed !".format(str(port)))
-
-	sock.close()
-	
-
+ 
+def scan():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ip = input("Enter IP to scan: ")
+        port = int(input("Port: "))
+        s.connect((ip, port))
+        print(port,"port is open.")
+	scan()
+    except:
+        print(port,"port is closed.")
+	scan()
+scan()
